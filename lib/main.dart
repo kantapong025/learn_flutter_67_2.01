39dec21fbf8b54a71be8c67392363040ab0c83c5
kantapong <kantapongnilakan@gmail.com>
2025-06-14 14:02:42 +0700

    step 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
-// Step 4 : sperate the Home widget into it own file
-// import 'screen/home.dart';
-
-import 'package:learn_flutter_67_2/screen/home.dart';
-
-
+// step4: sperate the Home widget into its own file
+import 'screen/home.dart';
 
 void main() {
   // Step 1: appBar and body
@@ -34,43 +30,11 @@ void main() {
       home: Scaffold(
         appBar: AppBar(
           title: Text("My App"),
-          backgroundColor: const Color.fromARGB(255, 243, 33, 33),
+          backgroundColor: const Color.fromARGB(255, 3, 221, 255),
           centerTitle: true,
         ),
         body: Home(),
       ),
     ),
   );
-}
-
-// Step 2: Stateless widget
-
-// class Home extends StatelessWidget {
-//   const Home({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Text("Hello Flutter");
-//   }
-
-//Step 3: container widget
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // return Container(color: Colors.blueAccent, width: 300, height: 300);
-    // return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10));
-    // return Container(color: Colors.blueAccent, 
-    // margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
-    // );
-    return Container(color: Colors.blueAccent,
-    margin: EdgeInsets.symmetric(horizontal: 20, vertical:  30),
-    padding: EdgeInsets.all(20),
-    child: Text(
-      "Hello Flutter",
-    style: TextStyle(fontSize: 30, letterSpacing: 3),
-    ),
-    );
-  }
 }
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,18 +1,59 @@
 //Step 4: sperate the Home widget into it own file
 import 'package:flutter/material.dart';
 
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  // Step 5:container, column,main axis, cross axis
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blueAccent,
-    margin: EdgeInsets.symmetric(horizontal: 20, vertical:  30),
-    padding: EdgeInsets.all(20),
-    child: Text(
-      "Hello Flutter",
-    style: TextStyle(fontSize: 30, letterSpacing: 3),
-    ),
+    return Column(
+      // mainAxisAlignment: MainAxisAlignment.start,
+      // mainAxisAlignment: MainAxisAlignment.center,
+      // mainAxisAlignment: MainAxisAlignment.end,
+      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: Colors.pinkAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              letterSpacing: 3,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.pinkAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              letterSpacing: 3,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.pinkAccent,
+          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 20,
+              letterSpacing: 3,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
