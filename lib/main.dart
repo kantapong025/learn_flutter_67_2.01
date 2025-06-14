0bf6d073b3871bc2a13e0ac88fe226088286eb07
kantapong <kantapongnilakan@gmail.com>
2025-06-14 13:53:20 +0700

    step 3

--- lib/main.dart
+++ lib/main.dart
@@ -36,13 +36,34 @@ void main() {
   );
 }
 
-// Step2
+// Step 2: Stateless widget
 
+// class Home extends StatelessWidget {
+//   const Home({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text("Hello Flutter");
+//   }
+
+//Step 3: container widget
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hello Flutter");
+    // return Container(color: Colors.blueAccent, width: 300, height: 300);
+    // return Container(color: Colors.blueAccent, margin: EdgeInsets.all(10));
+    // return Container(color: Colors.blueAccent, 
+    // margin: EdgeInsets.fromLTRB(5, 10, 15, 20),
+    // );
+    return Container(color: Colors.blueAccent,
+    margin: EdgeInsets.symmetric(horizontal: 20, vertical:  30),
+    padding: EdgeInsets.all(20),
+    child: Text(
+      "Hello Flutter",
+    style: TextStyle(fontSize: 30, letterSpacing: 3),
+    ),
+    );
   }
 }
